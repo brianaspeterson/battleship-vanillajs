f6bb0086fa99d386511049a2de724472108626fc
[Peterson, Briana] <briana.s.peterson@jpmchase.com>
2016-01-21 21:32:41 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var colArrayOne =[];
+var rowArrayOne = [];
+
+
+
+function test(){
+    var userOneInput = document.getElementById("userOneInput").value;
+    for (var i=0; i<userOneInput.length; i++){
+      rowArrayOne.push(userOneInput[i]);
+      colArrayOne.push(i);
+      rowArrayOne.push(userOneInput[i]+1);
+      colArrayOne.push(i);
+      rowArrayOne.push(userOneInput[i]+2);
+      colArrayOne.push(i);
+    }
+    var userTwoInput = document.getElementById("userTwoInput").value;
+    for (var j=0; i<userTwoInput.length; i++){
+      rowArrayOne.push(userTwoInput[j]);
+      colArrayOne.push(j);
+      rowArrayOne.push(userTwoInput[j]+1);
+      colArrayOne.push(j);
+      rowArrayOne.push(userTwoInput[j]+2);
+      colArrayOne.push(j);
+    }
+    
+}
+
+function begin(){
+
+	$('#turk').addClass('');
+
+
+}
